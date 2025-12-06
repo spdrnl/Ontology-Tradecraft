2280f66ca59044443b920ff21c81b45f8686648e
Sanne de Roever <sanne.de.roever@gmail.com>
2025-12-06 22:20:22 +0100

    Added to run_all.

--- projects/project-5/scripts/run_all.py
+++ projects/project-5/scripts/run_all.py
@@ -1,7 +1,11 @@
 from pathlib import Path
 import extract_definitions
+import filter_candidates_hybrid
 import preprocess_definitions_llm
 import logging
+
+import robot_elk
+import robot_merge
 from util.logger_config import config
 
 logger = logging.getLogger(__name__)
@@ -18,10 +22,21 @@ def main():
     logger.info("=======================================")
     logger.info("Running all scripts")
     logger.info("=======================================")
+
     logger.info("Extracting definitions...")
     extract_definitions.main()
+
     logger.info("Preprocessing definitions...")
     preprocess_definitions_llm.main()
+
+    logger.info("Filtering candidates ...")
+    filter_candidates_hybrid.main()
+
+    logger.info("Merging candidates ...")
+    robot_merge.main()
+
+    logger.info("Checking result with ELK ...")
+    robot_elk.main()
 
 
 if __name__ == "__main__":
